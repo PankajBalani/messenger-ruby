9ee4075e21225b0d1f65061ef46804ead2c36cb1
Pankaj <pankaj.balani@gmail.com>
2016-08-23 14:55:52 +0530

    Changed FacebookToken model to BotConfig inside messengers controller.

--- app/controllers/messenger/messenger_controller.rb
+++ app/controllers/messenger/messenger_controller.rb
@@ -47,10 +47,10 @@ module Messenger
 
     def set_page_access_token
       slug = params[:slug]
-      @facebook_token = FacebookToken.where(page_slug: slug).first
+      @facebook_token = BotConfig.where(page_slug: slug).first
       Messenger.configure do |config|
-        config.verify_token      = @facebook_token.verify_token
-        config.page_access_token = @facebook_token.page_access_token
+        config.verify_token      = @facebook_token.try(:verify_token)
+        config.page_access_token = @facebook_token.try(:page_access_token)
       end
     end
   end
