4ce8bf24fa980893cd3a1aaac3c3788d0bf93a80
Pankaj <pankaj.balani@gmail.com>
2016-09-29 14:41:02 +0530

    Added changes for QR working

--- lib/messenger/parameters/message.rb
+++ lib/messenger/parameters/message.rb
@@ -3,9 +3,12 @@ module Messenger
     class Message
       include Callback
 
-      attr_accessor :mid, :seq, :text, :attachments
+      attr_accessor :mid, :seq, :text, :attachments, :quick_reply, :app_id, :is_echo
 
-      def initialize(mid:, seq:, text: nil, attachments: nil)
+      def initialize(mid:, seq:, text: nil, attachments: nil, quick_reply: nil, is_echo: nil, app_id: nil)
+        @quick_reply = quick_reply if quick_reply.present?
+        @app_id      = app_id if app_id.present?
+        @is_echo     = is_echo if is_echo.present?
         @mid         = mid
         @seq         = seq
         @text        = text if text.present?
