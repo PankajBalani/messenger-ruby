8a86980d3611a5943337916aa29773524e4293eb
PankajBalani <pankaj.balani@gmail.com>
2017-02-07 18:06:01 +0530

    Support for sending location for quick replies

--- lib/messenger/components/elements/quick_reply.rb
+++ lib/messenger/components/elements/quick_reply.rb
@@ -1,16 +1,17 @@
 module Messenger
   module Elements
     class QuickReply
-      attr_accessor :content_type, :title, :payload
+      attr_accessor :content_type, :title, :payload, :image_url
 
-      def initialize(title:, payload:)
-        @content_type  = 'text'
+      def initialize(content_type:,title:, payload:, image_url:)
+        @content_type  = content_type
         @title = title
+        @image_url = image_url
         @payload = payload
       end
 
       def build
-        { content_type: @content_type, title: @title, payload: @payload }
+        { content_type: @content_type, title: @title, payload: @payload, image_url: @image_url}
       end
 
     end
