99258192a8ac548b0b0a96446b0bf5e407ba1dc9
Raminder Singh <ramindersahani@gmail.com>
2017-07-02 22:54:06 +0530

    platform dependent api call

--- lib/messenger/client.rb
+++ lib/messenger/client.rb
@@ -1,10 +1,17 @@
 module Messenger
   class Client
-    def self.get_user_profile(user_id)
-      JSON.parse(RestClient.get(
+    def self.get_user_profile(user_id,platform='messenger')
+      if platform == 'messenger'
+        JSON.parse(RestClient.get(
         "https://graph.facebook.com/v2.6/#{user_id}?fields=first_name,last_name,profile_pic,timezone,gender \
         &access_token=#{Messenger.config.page_access_token}",
       ))
+      elsif platform == 'workplace'
+        JSON.parse(RestClient.get(
+        "https://graph.facebook.com/v2.6/#{user_id}?fields=first_name,last_name,picture,timezone,gender \
+        &access_token=#{Messenger.config.page_access_token}",
+      ))
+      end  
     end
 
     def self.send(data)
